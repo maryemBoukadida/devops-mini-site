ad3d811b52054ca47a0b7502851e256762ac4407
maryemBoukadida <boukadida.maryem08@gmail.com>
2025-11-20 23:56:38 +0100

    Update smoke-test.bat

--- scripts/smoke-test.bat
+++ scripts/smoke-test.bat
@@ -1,10 +1,19 @@
 @echo off
-REM Test simple pour vérifier que le serveur Docker tourne
-curl -s http://localhost:8081  > nul
-IF %ERRORLEVEL% EQU 0 (
+setlocal enabledelayedexpansion
+
+set RETRY=0
+:CHECK
+curl -s http://localhost:8081 > nul
+if %ERRORLEVEL% EQU 0 (
     echo PASSED
     exit /b 0
-) ELSE (
-    echo FAILED
-    exit /b 1
+) else (
+    set /a RETRY+=1
+    if !RETRY! GEQ 10 (
+        echo FAILED
+        exit /b 1
+    )
+    echo Waiting for container to be ready... Retry !RETRY!
+    timeout /t 3 > nul
+    goto CHECK
 )
